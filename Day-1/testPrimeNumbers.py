8f7b8f714a73565dc3594e65579c698af5c848fe
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-07 00:06:04 +0300

    added the string test

--- Day-1/testPrimeNumbers.py
+++ Day-1/testPrimeNumbers.py
@@ -9,3 +9,10 @@ class TestSolution(unittest.TestCase):
 		self.assertTrue(type, int)
 	def test_one(self):
 		self.assertFalse(primeNumbers (num =1),)
+	def test_string(self):
+		self.assertTrue(type, int)
+	# def test_evenNumbers():
+	# 	pass
+
+if __name__ == '__main__':
+	unittest.main()
