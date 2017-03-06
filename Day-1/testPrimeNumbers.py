c0ec40d06765b8721b6d04af078823c4acbac786
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-06 23:20:12 +0300

    added the alphabet test

--- Day-1/testPrimeNumbers.py
+++ Day-1/testPrimeNumbers.py
@@ -5,3 +5,5 @@ class TestSolution(unittest.TestCase):
 	"""docstring for TestSolution"""
 	def test_negative(self):
 		self.assertFalse(primeNumbers (num = 0),)
+	def test_alphabet(self):
+		self.assertTrue(type, int)
