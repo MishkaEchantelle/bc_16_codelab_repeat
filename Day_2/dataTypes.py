6a763b82ee386d5741d9e337e74cb3c990f45f61
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-07 12:39:44 +0300

    data types solution

---
+++ Day_2/dataTypes.py
@@ -0,0 +1,24 @@
+def data_type(dataType):
+	if dataType == str:
+		return len(dataType)
+	if dataType == None:
+		return "no value"
+	if dataType == bool:
+		return dataType
+	if dataType == int:
+		if dataType < 100 :
+			return "less than 100"
+		elif dataType > 100
+			return "more than 100"
+		else
+			return "equal to 100"
+	if dataType == arrays:
+		if len(dataType) == 3:
+			return dataType[2]
+		else
+			return "undefine"
+	if dataType == functions:
+		if (dataType) == True
+			return dataType
+
+
