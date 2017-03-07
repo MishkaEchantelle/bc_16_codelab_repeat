8971271b8196f97795845125762cc2c316c24feb
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-07 16:55:25 +0300

    updating my files

--- Day_2/fizzBuzz.py
+++ Day_2/fizzBuzz.py
@@ -1,4 +1,4 @@
-def fizzBuzz(n):
+def fizz_buzz(n):
 
     if n % 3 == 0 and n % 5 == 0:
         return 'FizzBuzz'
@@ -8,4 +8,3 @@ def fizzBuzz(n):
         return 'Buzz'
     else:
         return str(n)
-
