26369fb9109e190c22c3a3a23272512280c17ad1
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-07 14:49:32 +0300

    car class solution

---
+++ Day_2/carClass.py
@@ -0,0 +1,11 @@
+class Car:
+	def __init__(self, type, model):
+		self.type = type
+		self.model = model
+		self.name = name
+
+	def vehicle(self):
+		return "{} {}".format(self.type, self.model, self.name)
+    
+
+    
