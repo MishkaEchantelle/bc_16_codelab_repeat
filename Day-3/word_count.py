0f41b0e2102958f27eb435f582b6be5276aabeda
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-08 19:19:26 +0300

    updated word count file

--- Day-3/word_count.py
+++ Day-3/word_count.py
@@ -1,25 +1,11 @@
-def maxMin():
-	 maximum = None
-	 minimum =None
+def word_count(wordCount):
+	wordCount = {}
 
-	 while True:
-	 	input = raw_input("Enter a number:")
-
-	 	if input == "done":
-	 		break
-	 	if len(input) < 1:
-	 		break
-
-	 	try:
-	 		number = int(input)
-	 	except :
-	 		print"Invalid input"
-	 		continue
-
-	 	if minimum is None:
-	 		minimum = number
-	 		maximum = number
-	 	elif number < minimum:
-	 		minimum = number
-	 	elif number > maximum:
-	 	    maximum = number
+	for word in wordCount.read().split():
+		if word not in wordCount:
+			wordCount[word] = 1
+		else:
+			wordCount[word] += 1
+	for key in wordCount.key():
+		print("%s %s " %(key , wordCount[key]))
+	word.close():
