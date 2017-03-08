7955c9dc02a74992cf1f81d2a7f52698a10c5203
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-08 21:53:45 +0300

    updated car class file

--- Day_2/carClass.py
+++ Day_2/carClass.py
@@ -1,10 +1,40 @@
-class Car:
-	def __init__(self, type, model, name):
-		self.type = type
-		self.model = model
-		self.name = name
-		
-		self.list = []
-		
-		def add_list (self, list)
-		  self.lists.append(list)
+
+class Car(object):
+
+    def __init__(self, name='General', model='GM', car_type='saloon'):
+
+        self.name = name
+        self.model = model
+        self.car_type = car_type
+        
+        
+        self.speed = 0
+        self.num_of_doors = self.get_num_of_doors()
+        self.num_of_wheels = self.get_num_of_wheels()
+
+    def is_saloon(self):
+
+        return self.car_type == 'saloon'
+
+    def get_num_of_doors(self):
+
+        if self.name == 'Porshe' or self.name == 'Koenigsegg':
+            return 2
+        else:
+            return 4
+
+    def get_num_of_wheels(self):
+
+        if self.car_type == 'trailer':
+            return 8
+        else:
+            return 4
+
+    def drive(self, num):
+
+        if self.car_type == 'saloon':
+            self.speed = 10 ** num
+        else:
+            self.speed = 77
+
+        return self
