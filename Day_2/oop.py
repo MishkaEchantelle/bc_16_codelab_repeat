057d5f95e4a9879d53d50cde86e0b242ec0f6d1f
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-09 23:43:17 +0300

    updated my OOP file

--- Day_2/oop.py
+++ Day_2/oop.py
@@ -1,6 +1,22 @@
 class Truck(object):
 	"""docstring for Truck"""
-	def __init__(self, arg):
+	def __init__(self, year, model, make, miles, wheels, sold_on):
+
+		self.year = year
+		self.model = model
+		self.make = make
+		self.miles = miles
+		self.wheels = wheels
+		self.sold_on = sold_on
 		
-		self.arg = arg
-		
+	def sale_price(self):
+		if self.sold_on is not None:
+			return 0.0
+		else:
+			return float * self.wheels
+
+	def purchase_price(self):
+		if self.sold_on is None:
+			return 0.0
+		else:
+			return 10000 - (.10 * self.miles)
