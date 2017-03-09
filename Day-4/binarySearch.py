ea9a7b117a86e6c606b5fb38d13235424c34e7de
MishkaEchantelle <echantelle.mishka@gmail.com>
2017-03-09 12:42:35 +0300

    updated Binary search file

--- Day-4/binarySearch.py
+++ Day-4/binarySearch.py
@@ -2,5 +2,9 @@ class BinarySearch(object):
 	"""docstring for BinarySearch"""
 	def __init__(self, a, b):
 		super(BinarySearch, self).__init__()
-		self.a = a
+		
+		self.a = len(self)
+		
+		for x in range(1, a+1):
+			self.append(x * b)
 		
